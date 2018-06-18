398729ec0c6bbcf50c2f70284dadde22cf3c673d
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-06-18 19:28:54 -0400

    updates

--- liri.js
+++ liri.js
@@ -1,111 +1,156 @@
-require("dotenv").config();
-var fs = require("fs");
-var Twitter = require('twitter');
-var request = require("request");
-var Spotify = require('node-spotify-api');
-var Keys = require("./key.js");
+  require("dotenv").config();
+  var fs = require("fs");
+  var Twitter = require('twitter');
+  var request = require("request");
+  var Spotify = require('node-spotify-api');
+  var Keys = require("./key.js");
 
 
-fs.readFile("key.js", "utf8", function(error, data) {
- 
-var a = process.argv[2]
+  // fs.readFile("key.js", "utf8", function(error, data) {
+  
+  var a = process.argv[2]
 
 
-if (a === "my-tweets") {
+  if (a === "my-tweets") {
 
-var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
-   
-  var params = {screen_name: "MyTweet18518181"};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      for (i=0; i <= 4; i++) {
-      console.log(tweets[i].text);
+  var client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+    
+    var params = {screen_name: "MyTweet18518181"};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        for (i=0; i <= 4; i++) {
+        console.log(tweets[i].text);
+        }
       }
-    }
-  });
-}
-
-//node liri.js spotify-this-song '<song name here>'
-
-if (a === "spotify-this-song") {
-
-    var b = process.argv[3]
- 
-var spotify = new Spotify({
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-});
- 
-spotify.search({ type: 'track', query: b }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
+    });
   }
 
-  //this returns weird data
-    console.log(data.tracks.items[0].artists[0]); 
-});
+  //node liri.js spotify-this-song '<song name here>'
 
-} else if (b === false) {
+  if (a === "spotify-this-song") {
 
-    var spotify = new Spotify({
-        id: process.env.SPOTIFY_ID,
-        secret: process.env.SPOTIFY_SECRET
-      });
-       
-      spotify.search({ type: 'track', query: 'the sign' }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
+      var b = process.argv[3]
+  
+  var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  });
+  
+  spotify.search({ type: 'track', query: b }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    var songData = data.tracks.items;
+    
+      console.log("artist: " + songData[0].album.artists[0].name);
+      console.log("song: " + songData[0].name);
+      console.log("preview: " + songData[0].preview_url);
+      console.log("album: " + songData[0].album.name);
+     
+  });
+  }
+  else if (b === "" && a === "spotify-this-song") {
+
+      var spotify = new Spotify({
+          id: process.env.SPOTIFY_ID,
+          secret: process.env.SPOTIFY_SECRET
+        });
+        
+        spotify.search({ type: 'track', query: 'the sign' }, function(err, data) {
+          if (err) {
+            return console.log('Error occurred: ' + err);
+          }
+
+          var songData = data.tracks.items;
+    
+          console.log("artist: " + songData[0].album.artists[0].name);
+          console.log("song: " + songData[0].name);
+          console.log("preview: " + songData[0].preview_url);
+          console.log("album: " + songData[0].album.name);
+
+  })};
+
+  if (a === "movie-this") {
+
+  var title = process.argv[3];
+  
+  if (title === ""){
+    title = "mr.nobody"
+  }
+  request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+      if (!error) {
+          // * Title of the movie.
+      console.log(body[0].Title);
+      //  * Year the movie came out.
+      console.log(body.Year);
+      //  * IMDB Rating of the movie.
+      console.log(body.imdbRating);
+      //  * Rotten Tomatoes Rating of the movie.
+      console.log(body.Ratings);
+      //  * Country where the movie was produced.
+      console.log(body.Country);
+      //  * Language of the movie.
+      console.log(body.Language);
+      //  * Plot of the movie.
+      console.log(body.Plot);
+      //  * Actors in the movie.
+      console.log(body.Actors);
+          // return console.log(body.Title + body.Year + body.imdbRating + body.Ratings + body.Country + body.Language + body.Plot + body.Actors);
+        } else {
+
+        console.log("error");
+
         }
 
-        //this returns error message
-        console.log(data)
-})}});
+  })
 
-if (a === "movie-this") {
+  };
 
-var title = process.argv[3];
+  var a = process.argv[2];
 
-request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+  if (a === "do-what-it-says") {
 
-    if (!error) {
-      //this does nothing
-        return console.log(body);
-        // return console.log(body.Title + body.Year + body.imdbRating + body.Ratings + body.Country + body.Language + body.Plot + body.Actors);
-      }
+      fs.readFile("random.txt", "utf8", function(error, data) {
+          console.log(data);
+                   
+          if (process.argv[0] == "spotify-this-song") {
 
-})
+            var x = process.argv[1]
 
-if (title === "") {
+            var spotify = new Spotify({
+              id: process.env.SPOTIFY_ID,
+              secret: process.env.SPOTIFY_SECRET
+            });
+            
+            spotify.search({ type: 'track', query: x }, function(err, data) {
+              if (err) {
+                return console.log('Error occurred: ' + err);
+              }
+          
+              var songData = data.tracks.items;
+              
+                console.log("artist: " + songData[0].album.artists[0].name);
+                console.log("song: " + songData[0].name);
+                console.log("preview: " + songData[0].preview_url);
+                console.log("album: " + songData[0].album.name);
+               
+            });
+            
 
-  request("http://www.omdbapi.com/?t=it&y=&plot=short&apikey=trilogy", function(error, response, body) {
+          }
+      })
 
-    if (!error) {
+  }
 
-      console.log(body);
+  // * `do-what-it-says`
+  // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 
-    }
-});
-
-};
-
-};
-
-var a = process.argv[2];
-
-if (a === "do-what-it-says") {
-
-    fs.readFile("random.txt", "utf8", function(error, data) {
-        console.log(data);
-    })
-}
-
-// * `do-what-it-says`
-// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-// Feel free to change the text in that document to test out the feature for other commands.
+  // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+  // Feel free to change the text in that document to test out the feature for other commands.
